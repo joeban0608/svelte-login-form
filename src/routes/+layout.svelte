7e3c9020842f34c612f7e03aa45f4a766a5e48bf
joeban <joeban@twbeex.com>
2024-07-30 14:48:29 +0800

    refactor: header navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,69 +3,37 @@
 	import '../app.css';
 
 	export let data: LayoutData;
+	import { page } from '$app/stores';
 </script>
 
 <header class="w-full">
-	<ul class="menu bg-base-200 lg:menu-horizontal rounded-box w-full rounded-none">
-		<li>
-			<a href="/">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					class="h-5 w-5"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke="currentColor"
+	<div class="navbar bg-base-100">
+		<div class="flex-1">
+			<a href="/" class="btn btn-ghost text-xl">Home</a>
+		</div>
+		<div class="flex-none">
+			{#if $page.url.pathname !== '/login'}
+				<a href="/login">
+					<div class="mr-4 btn text-info">Login</div>
+				</a>
+			{/if}
+			<div class="dropdown dropdown-end">
+				<div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
+					<div class="w-10 rounded-full">
+						<img
+							alt="Tailwind CSS Navbar component"
+							src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+						/>
+					</div>
+				</div>
+				<ul
+					class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
 				>
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-						d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-					/>
-				</svg>
-				Home
-			</a>
-		</li>
-		<li>
-			<a href="/login">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					width="24"
-					height="24"
-					viewBox="0 0 24 24"
-					fill="none"
-					stroke="currentColor"
-					stroke-width="2"
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					class="lucide lucide-log-out"
-					><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" /><polyline
-						points="16 17 21 12 16 7"
-					/><line x1="21" x2="9" y1="12" y2="12" /></svg
-				>
-				Login
-			</a>
-		</li>
-		<li>
-			<a href="/#">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					width="24"
-					height="24"
-					viewBox="0 0 24 24"
-					fill="none"
-					stroke="currentColor"
-					stroke-width="2"
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					class="lucide lucide-log-out"
-					><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" /><polyline
-						points="16 17 21 12 16 7"
-					/><line x1="21" x2="9" y1="12" y2="12" /></svg
-				>
-				Logout
-			</a>
-		</li>
-	</ul>
+					<li><a href="/#setting">Settings</a></li>
+					<li><a href="/">Logout</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
 </header>
 <slot />
